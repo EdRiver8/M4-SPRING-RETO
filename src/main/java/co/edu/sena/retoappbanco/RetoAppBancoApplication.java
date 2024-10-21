8453153818c5c2d10fa58007c099f139ca9d7590
EdRiver8 <dwein08@gmail.com>
2024-10-21 08:24:16 -0500

    Creando la base del proyecto

---
+++ src/main/java/co/edu/sena/retoappbanco/RetoAppBancoApplication.java
@@ -0,0 +1,13 @@
+package co.edu.sena.retoappbanco;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RetoAppBancoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RetoAppBancoApplication.class, args);
+    }
+
+}
